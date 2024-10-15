9034e0134a57aab789aa6412a70e253da5007acf
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-10-15 20:01:07 +0530

    Connected to mongodb atlas

--- Server/config/mongoose.js
+++ Server/config/mongoose.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 main().catch(err => console.log(err));
 
 async function main() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/WhatsApp');
+        // await mongoose.connect('mongodb://localhost:27017/WhatsApp');
+        await mongoose.connect(process.env.DB_URI);  // Connection to the database
         console.log("Database connection successful...");  // Connection success message
     } catch (err) {
         console.error('Problem connecting to the database', err);  // Connection error handling
